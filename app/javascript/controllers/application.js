0446a633da07d22c113eb0aba1b73c7aa7e62be4
Conor Graves <cg00853@surrey.ac.uk>
2023-03-02 13:12:37 +0000

    Fly.io setup and google maps setup

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,6 +4,27 @@ const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+window.initMap = initMap;
+
+
 
 export { application }
+
+
+/*
+window.fireMapsLoadedEvent = function () {
+    const evt = new Event("mapsLoaded")
+    document.dispatchEvent(evt)
+}
+*/
+
+//let map; 
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
+
---
+++ app/javascript/controllers/maps_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    /*
+    static target = ["coordinates"]
+
+    
+    connect() {
+        if (typeof google != "undefined") {
+            this.renderMap;
+            this.element.textContent = "Hello World!"
+            
+        }
+    }
+
+    renderMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8,
+        });
+    }
+
+*/
+}
