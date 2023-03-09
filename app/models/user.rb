203d8aec83b4edca08359a8d34b0b4a56e277a97
Conor Graves <cg00853@surrey.ac.uk>
2023-03-09 16:55:02 +0000

    Part 1 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,25 @@
 class User < ApplicationRecord
 
-	belongs_to :account, :inverse_of => :user
+	# Account
+	belongs_to :account, :inverse_of => :users
 	accepts_nested_attributes_for :account
+
+	# Trainers
+	has_many :trainers, dependent: :destroy, :inverse_of => :user
+	accepts_nested_attributes_for :trainers
+
+	has_many :schedules, dependent: :destroy, :inverse_of => :user
+	accepts_nested_attributes_for :schedules
+
+	# Clients
+	has_many :clients, dependent: :destroy, :inverse_of => :user
+	accepts_nested_attributes_for :clients
+ 
+	# Lessons
+	has_many :bookings, dependent: :destroy, :inverse_of => :user
+	accepts_nested_attributes_for :bookings
+ 
+	has_many :lesson_payments, dependent: :destroy, :inverse_of => :user
+	accepts_nested_attributes_for :lesson_payments
 
 end
