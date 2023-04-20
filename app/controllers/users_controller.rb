29568714cb1501af2a98e8e08aaf41345d3c807c
Conor Graves <cg00853@surrey.ac.uk>
2023-04-20 17:25:35 +0100

    Added devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
+    @user.branch_id = nil
 
     respond_to do |format|
       if @user.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-belongs_to :branch
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+belongs_to :branch, optional: true
 has_many :availabilities, dependent: :destroy
 has_many :deliveries, dependent: :destroy
 
