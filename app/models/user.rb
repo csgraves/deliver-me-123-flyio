91f00e319a43f2f6c794b12ca122071ffc393ddb
Conor Graves <cg00853@surrey.ac.uk>
2023-04-25 15:30:23 +0100

    Set defualt role for user to 'admin'

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-belongs_to :branch, optional: true
-has_many :availabilities, dependent: :destroy
-has_many :deliveries, dependent: :destroy
+  belongs_to :branch, optional: true
+  has_many :availabilities, dependent: :destroy
+  has_many :deliveries, dependent: :destroy
 
+  before_validation :set_default_role, on: :create
+
+  private
+
+  def set_default_role
+    self.role ||= :admin
+  end
 end
