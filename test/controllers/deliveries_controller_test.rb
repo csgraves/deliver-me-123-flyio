a3f3d7a14e6838050d0f5a0d2d6a4ef510fe77dc
Conor Graves <cg00853@surrey.ac.uk>
2023-05-12 12:59:46 +0100

    Deliveries scaff and tests added

---
+++ test/controllers/deliveries_controller_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+class DeliveriesControllerTest < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
+  setup do
+    @delivery = deliveries(:one)
+    sign_in users(:one)
+  end
+
+  test "should get index" do
+    get deliveries_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_delivery_url
+    assert_response :success
+  end
+
+  test "should create delivery" do
+    assert_difference("Delivery.count") do
+      post deliveries_url, params: { delivery: { deliver: @delivery.deliver, leave: @delivery.leave, schedule_id: @delivery.schedule_id } }
+    end
+
+    assert_redirected_to delivery_url(Delivery.last)
+  end
+
+  test "should show delivery" do
+    get delivery_url(@delivery)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_delivery_url(@delivery)
+    assert_response :success
+  end
+
+  test "should update delivery" do
+    patch delivery_url(@delivery), params: { delivery: { deliver: @delivery.deliver, leave: @delivery.leave, schedule_id: @delivery.schedule_id } }
+    assert_redirected_to delivery_url(@delivery)
+  end
+
+  test "should destroy delivery" do
+    assert_difference("Delivery.count", -1) do
+      delete delivery_url(@delivery)
+    end
+
+    assert_redirected_to deliveries_url
+  end
+end
