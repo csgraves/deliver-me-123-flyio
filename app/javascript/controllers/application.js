12350d88b224903ffeaad903f821e9a09849f207
Conor Graves <cg00853@surrey.ac.uk>
2023-06-16 17:50:29 +0100

    jsbundling-rails setup

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,6 +2,11 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
+import controllers from "./**/*_controller.js"
+controllers.forEach((controller) => {
+    application.register(controller.name, controller.module.default)
+})
+
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus = application
