df5f0b8841175a0c2e88e5a03edfc878b40171f6
Conor Graves <cg00853@surrey.ac.uk>
2023-07-27 12:02:51 +0100

    Deliveries controller tests added

--- test/controllers/deliveries_controller_test.rb
+++ test/controllers/deliveries_controller_test.rb
@@ -69,4 +69,60 @@ class DeliveriesControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to deliveries_url
   end
+
+  test "should not create delivery with invalid data" do
+    assert_no_difference("Delivery.count") do
+      post deliveries_url, params: { delivery: { 
+        origin_leave: nil,
+        schedule_id: @delivery.schedule_id,
+        dest_arrive: @delivery.dest_arrive,
+        dest_leave: @delivery.dest_leave
+      } }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "should not update delivery with invalid data" do
+    patch delivery_url(@delivery), params: { delivery: { 
+        origin_leave: @delivery.origin_leave,
+        schedule_id: @delivery.schedule_id,
+        dest_arrive: nil,
+        dest_leave: @delivery.dest_leave
+        } }
+
+    assert_response :unprocessable_entity
+  end
+
+  test "should not get edit for non-admin users" do
+    sign_out users(:one)
+    sign_in users(:two)
+
+    get edit_delivery_url(@delivery)
+    assert_redirected_to root_path
+  end
+
+  test "should not update delivery for non-admin users" do
+    sign_out users(:one)
+    sign_in users(:two)
+
+    patch delivery_url(@delivery), params: { delivery: { 
+        origin_leave: @delivery.origin_leave,
+        schedule_id: @delivery.schedule_id,
+        dest_arrive: @delivery.dest_arrive,
+        dest_leave: nil
+        } }
+
+    assert_redirected_to root_path
+  end
+
+  test "should not destroy delivery for non-admin users" do
+    sign_out users(:one)
+    sign_in users(:two)
+
+    assert_no_difference("Delivery.count") do
+      delete delivery_url(@delivery)
+    end
+
+    assert_redirected_to root_path
+  end
 end
