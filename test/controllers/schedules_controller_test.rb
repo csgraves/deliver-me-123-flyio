ab228883aa308e20d574a50fdaa560d69de4a884
Conor Graves <cg00853@surrey.ac.uk>
2023-07-27 10:08:02 +0100

    Schedules controller tests added

--- test/controllers/schedules_controller_test.rb
+++ test/controllers/schedules_controller_test.rb
@@ -53,4 +53,36 @@ class SchedulesControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to schedules_url
   end
+
+  test "should not create schedule if user already has an existing schedule" do
+    user_with_existing_schedule = users(:one)
+    schedule_params = {
+      user_id: user_with_existing_schedule.id,
+      user_only: true
+    }
+
+    assert_no_difference("Schedule.count") do
+      post schedules_url, params: { schedule: schedule_params }
+    end
+
+    assert_response :redirect
+    assert_redirected_to new_schedule_url
+    assert_match /User already has an existing schedule\./, flash[:alert]
+  end
+
+  test "should not create schedule if branch already has an existing schedule" do
+    branch_with_existing_schedule = branches(:two)
+    schedule_params = {
+      branch_id: branch_with_existing_schedule.id,
+      branch_only: true
+    }
+
+    assert_no_difference("Schedule.count") do
+      post schedules_url, params: { schedule: schedule_params }
+    end
+
+    assert_response :redirect
+    assert_redirected_to new_schedule_url
+    assert_match /Branch already has an existing schedule\./, flash[:alert]
+  end
 end
