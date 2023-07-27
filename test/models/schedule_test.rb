d0cf3ecce69a9242a04928fe64a32f7473bd2e21
Conor Graves <cg00853@surrey.ac.uk>
2023-07-27 09:59:32 +0100

    Schedules model tests added

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class ScheduleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "schedule with user should be valid" do
+    user = users(:one)  # Assuming you have fixture data for users
+    schedule = Schedule.new(start_time: Time.now, end_time: Time.now + 1.hour, user: user)
+    assert schedule.valid?
+  end
+
+  test "schedule with branch should be valid" do
+    branch = branches(:one)  # Assuming you have fixture data for branches
+    schedule = Schedule.new(start_time: Time.now, end_time: Time.now + 1.hour, branch: branch)
+    assert schedule.valid?
+  end
+
+  test "schedule without user or branch should be invalid" do
+    schedule = Schedule.new(start_time: Time.now, end_time: Time.now + 1.hour)
+    assert_not schedule.valid?
+    assert_includes schedule.errors[:base], "Schedule must belong to a user or a branch"
+  end
 end
